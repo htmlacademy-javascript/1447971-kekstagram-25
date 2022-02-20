60f3d1a2b79f3be7aae9c0fbad80cc91c2f4d962
masha8709 <masha.8709@mail.ru>
2022-02-20 20:52:50 +0300

    Исправила ошибки, которые показал github

--- js/main.js
+++ js/main.js
@@ -2,16 +2,19 @@
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 function getRandomIntInclusive(min, max) {
-  if (max < min || min == max || min < 0) {
-    console.log(`Диапазон может быть только положительный, минимум должен быть от 0, максимум должен быть больше минимума`);
-  }
+  //ESlint на github выдаёт ошибку, что нельзя использовать console, поэтому закомментировала,
+  //я не знаю, куда ещё можно ошибку вывести
+
+  // if (max < min || min === max || min < 0) {
+  //   console.log(`Диапазон может быть только положительный, минимум должен быть от 0, максимум должен быть больше минимума`);
+  // }
 
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(getRandomIntInclusive(9, 11));
+getRandomIntInclusive(9, 11);
 
 //Эту функцию не брала с сайта, написала сама.
 //Функция для проверки максимальной длины строки
@@ -20,4 +23,4 @@ function getLength (description, length) {
   return description.length <= length ? true : false;
 }
 
-console.log(getLength('Текстовый комментарий', 140));
+getLength('Текстовый комментарий', 140);
