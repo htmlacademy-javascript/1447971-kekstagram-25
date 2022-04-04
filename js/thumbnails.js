1a7cc77856c0d872d93d48ce3ec3aaccba877f12
masha8709 <masha.8709@mail.ru>
2022-04-04 23:25:58 +0300

    отрисовка фото пользователей

---
+++ js/thumbnails.js
@@ -0,0 +1,19 @@
+import {createDescriptions} from './create-description.js';
+
+const BLOG_PICTURE = document.querySelector('.pictures');
+
+const TEMPLATE_PICTURE = document.querySelector('#picture').content.querySelector('.picture');
+
+const USER_PICTURES = createDescriptions();
+
+const PICTURE_FRAGMENT = document.createDocumentFragment();
+
+USER_PICTURES.forEach((url, likes, comments) => {
+  const PICTURE_ELEMENT = TEMPLATE_PICTURE.cloneNode(true);
+  PICTURE_ELEMENT.querySelector('.picture__img').src = url;
+  PICTURE_ELEMENT.querySelector('.picture__likes').textContent = likes;
+  PICTURE_ELEMENT.querySelector('.picture__comments').textContent = comments;
+  PICTURE_FRAGMENT.appendChild(PICTURE_ELEMENT);
+});
+
+BLOG_PICTURE.appendChild(PICTURE_FRAGMENT);
